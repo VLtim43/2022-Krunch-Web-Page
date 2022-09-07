9da3d83a9cdecc4ecf61a7f32504ea10ec9fe8df
fernando <fernandoreal.valentim@gmail.com>
2022-09-07 16:30:40 -0300

    Initial component structre

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import GlobalStyle from "./styles/global"
+import Link from "./components/micro/link/Link"
 
 function App() {
   return (
     <div className="App">
        <GlobalStyle/>
-       
+       <Link/>
     </div>
   )
 }
---
+++ src/components/micro/link/Link.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type Props = { text ?: string }; 
+
+const Link = ({ text }: Props) => 
+
+<div>{text}</div>;
+
+
+export default Link
