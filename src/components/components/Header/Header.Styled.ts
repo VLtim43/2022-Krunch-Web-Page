ce8616e08fc3b8da7cff143444d59e83457f53ed
fernando <fernandoreal.valentim@gmail.com>
2022-09-08 21:48:00 -0300

    Header margins

--- src/components/components/Header/Header.Styled.ts
+++ src/components/components/Header/Header.Styled.ts
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
-export const HeaderWrapper = styled.div`
-  
+export const HeaderWrapper = styled.header`
+  background-color: rgba(34, 34, 34, .9);
+  display: flex;
+  justify-content: center;
+ 
+  .header-div {
+   display: flex;
+   justify-content:space-between;
+   flex-direction: row;
+   margin-left: 4.6rem;
+   margin-right: 4.6rem;
+   width: 100%;
+   max-width: 1300px;
+
+   
+  }
+
+  nav {
+    background-color: red;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  a{
+    margin: 30px 20px;
+  }
+
+  img{
+    margin: 30px 20px;
+  }
+ 
+ 
 `;
